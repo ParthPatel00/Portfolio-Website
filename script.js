3249a0e7e94b1cc15541793dd8cdfcf3afcb7d64
Parth Patel <54247833+ParthPatel00@users.noreply.github.com>
2021-11-11 03:55:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    //go to top script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+
+    });
+
+    // toggle menu/navbar upon click
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+
+    // backspace animation on homepage using script
+    var typed = new Typed(".typing",{
+        strings: ["Data Science Analyst", "Machine Learning Developer", "Software Developer"],
+        typeSpeed: 100,
+        backSpeed: 20,
+        loop: true
+    })
+
+    var typed = new Typed(".typing-small",{
+        strings: ["Data Science Analyst", "Machine Learning Developer", "Software Developer"],
+        typeSpeed: 100,
+        backSpeed: 20,
+        loop: true
+    })
+
+    var typed = new Typed(".typing-major",{
+        strings: ["Mathematics", "Statistics"],
+        typeSpeed: 100,
+        backSpeed: 20,
+        loop: true
+    })
+
+});
+
+
